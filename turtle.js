4bead75d092348455110e5317961079a8632becc
ViniciusPrincipe <vbmp@cin.ufpe.br>
2023-02-26 11:32:28 -0300

    Adição de atributo e metodo pra armazenar o caminho percorrido

--- turtle.js
+++ turtle.js
@@ -1,4 +1,4 @@
-class Turtle(){
+class Turtle{
     constructor(x,y){
         this.pos = createVector(x, y);
         this.vel = createVector(0, 0);
@@ -6,18 +6,23 @@ class Turtle(){
         this.maxSpeed = 4;
         this.maxForce = 0.5;
         this.r = 16;
+        this.visited = [];
     }
 
     seek(food) {
         let force = p5.Vector.sub(food, this.pos);
-        force.setMag(this.maxSpeed)
+        force.setMag(this.maxSpeed);
         force.sub(this.vel);
         force.limit(this.maxForce);
         return force;
     }
 
     applyForce(force){
-        this
+        this.acc.add(force)
+    }
+
+    addVisited(){
+        this.visited.push(this.pos)
     }
 
 }
