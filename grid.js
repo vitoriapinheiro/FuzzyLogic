5aad099e2ce1d191e046584287ebc1a7fdda15ac
arthur <arthurmedeiros32@gmail.com>
2023-02-26 23:00:51 -0300

    correct vini solution

--- grid.js
+++ grid.js
@@ -1,97 +1,98 @@
-class Grid{
-    constructor(linha,coluna,cellSize){
-        this.linha = linha;
-        this.coluna = coluna;
-        this.cellSize = cellSize;
-        this.gridMatrix = [];
-        
-        for(let i = 0; i < this.coluna; i++){
-            
-            let col = [];
-            for(let j = 0; j < this.linha; j++)col.push({title: "vazio", weight: 0});
+class Grid {
+  constructor(linha, coluna, cellSize) {
+    this.linha = linha;
+    this.coluna = coluna;
+    this.cellSize = cellSize;
+    this.gridMatrix = [];
 
-            this.gridMatrix.push(col);
-        }
-    }
-    drawGrid(){
-        for(let i = 0; i < this.coluna; i++){
-            for(let j = 0; j < this.linha;j++){
-                switch(this.gridMatrix[i][j].title){
-                    case "vazio" :
-                        fill("255");
-                        break;
-                    
-                    case "grass":
-                        fill(0,255,0);
-                        break;
-                    
-                    case "water" :
-                        fill(0, 0 ,255);
-                        break;
-                    
-                    case "parede" :
-                        fill('#222222')
-                        break;
-                    
-                  case "comida" :
-                        fill('#fae');
-                        break;
-                  
-                  case "agente" :
-                        fill('#ff0');
-                        break;
-                }  
-                rect(i * this.cellSize, j * this.cellSize, this.cellSize, this.cellSize);
-    
-    
-            }
-        }
-    }
-    addObstaculos(grassCount, waterCount,paredeCount){
-        
-        for(let i = 0; i < grassCount; i++){
-            let x = int(random(this.coluna));
-            let y = int(random(this.linha));
-            this.gridMatrix[x][y] = {title: "grass", weight: 5};
-        }
+    for (let i = 0; i < this.coluna; i++) {
+      let col = [];
+      for (let j = 0; j < this.linha; j++)
+        col.push({ title: "vazio", weight: 0 });
 
-        for(let i = 0; i < grassCount; i++){
-            let x = int(random(this.coluna));
-            let y = int(random(this.linha));
-            this.gridMatrix[x][y] = {title: "water", weight: 10};
-        }
-      
-
-        for(let i = 0; i < paredeCount; i++){
-            let x = int(random(this.coluna));
-            let y = int(random(this.linha));
-            this.gridMatrix[x][y] = {title: "parede", weight: Infinity};
-        }
+      this.gridMatrix.push(col);
     }
-    addFood(){
-        let x = Math.floor(int(random(this.coluna)));
-        let y = Math.floor(int(random(this.linha)));
-        while(1){
-          if(this.gridMatrix[x][y].title === "vazio"){
-             this.gridMatrix[x][y] = {title: "comida", weight: 0};
+  }
+  drawGrid() {
+    for (let i = 0; i < this.coluna; i++) {
+      for (let j = 0; j < this.linha; j++) {
+        switch (this.gridMatrix[i][j].title) {
+          case "vazio":
+            fill("255");
             break;
-            }
-          x = Math.floor(int(random(this.coluna)));
-          y = Math.floor(int(random(this.linha)));
-        }
-      return {x, y};
-    }
-  addAgent(){
-        let x = Math.floor(int(random(this.coluna)));
-        let y = Math.floor(int(random(this.linha)));
-        while(1){
-          if(this.gridMatrix[x][y].title === "vazio"){
-             this.gridMatrix[x][y] = {title: "agente", weight: Infinity};
+
+          case "grass":
+            fill(0, 255, 0);
             break;
-            }
-          x = Math.floor(int(random(this.coluna)));
-          y = Math.floor(int(random(this.linha)));
+
+          case "water":
+            fill(0, 0, 255);
+            break;
+
+          case "parede":
+            fill("#222222");
+            break;
+
+          case "comida":
+            fill("#fae");
+            break;
+
+          case "agente":
+            fill("#ff0");
+            break;
         }
-      return {x, y};
+        rect(
+          i * this.cellSize,
+          j * this.cellSize,
+          this.cellSize,
+          this.cellSize
+        );
+      }
+    }
+  }
+  addObstaculos(grassCount, waterCount, paredeCount) {
+    for (let i = 0; i < grassCount; i++) {
+      let x = int(random(this.coluna));
+      let y = int(random(this.linha));
+      this.gridMatrix[x][y] = { title: "grass", weight: 5 };
+    }
+
+    for (let i = 0; i < grassCount; i++) {
+      let x = int(random(this.coluna));
+      let y = int(random(this.linha));
+      this.gridMatrix[x][y] = { title: "water", weight: 10 };
+    }
+
+    for (let i = 0; i < paredeCount; i++) {
+      let x = int(random(this.coluna));
+      let y = int(random(this.linha));
+      this.gridMatrix[x][y] = { title: "parede", weight: Infinity };
+    }
+  }
+  addFood() {
+    let x = Math.floor(int(random(this.coluna)));
+    let y = Math.floor(int(random(this.linha)));
+    while (1) {
+      if (this.gridMatrix[x][y].title === "vazio") {
+        this.gridMatrix[x][y] = { title: "comida", weight: 0 };
+        break;
+      }
+      x = Math.floor(int(random(this.coluna)));
+      y = Math.floor(int(random(this.linha)));
+    }
+    return { x, y };
+  }
+  addAgent() {
+    let x = Math.floor(int(random(this.coluna)));
+    let y = Math.floor(int(random(this.linha)));
+    while (1) {
+      if (this.gridMatrix[x][y].title === "vazio") {
+        this.gridMatrix[x][y] = { title: "agente", weight: Infinity };
+        break;
+      }
+      x = Math.floor(int(random(this.coluna)));
+      y = Math.floor(int(random(this.linha)));
+    }
+    return { x, y };
   }
 }
--- world.js
+++ world.js
@@ -4,17 +4,14 @@ class World {
     let { x, y } = this.grid.addFood();
     let { i, j } = this.grid.addAgent();
     this.food = new Food(x, y);
-    this.agent = new Agent(i, j, this.grid);
+    this.agent = new Agent(i, j,this.grid,);
   }
 
   run() {
     this.grid.drawGrid();
     this.agent.move();
-
+    
     // let nextPosition = move()// posição do próximo nó no caminho calculado pelo algoritmo de busca
-    this.agent.setTargetPosition(
-      this.agent.currentPosition.x + 1,
-      this.agent.currentPosition.y
-    );
+    this.agent.setTargetPosition(this.agent.currentPosition.x+1, this.agent.currentPosition.y);
   }
 }
